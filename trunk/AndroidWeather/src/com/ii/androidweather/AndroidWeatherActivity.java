e8da8187e568b270a8f0b072e2475106080edf6f
nikola.ar05 <nikola.ar05@f99e8272-aff1-9975-84a3-243bb2b3cfa2>
2010-04-25 21:47:03 +0000

    Имплементирање на onCreate . Во него имаме парсирање на XML документ вратен од Google Weather api за константна локација пр.Скопје, со помош на SAXParser. И прикажување на основни информации температура и состојба за скопје.

--- trunk/AndroidWeather/src/com/ii/androidweather/AndroidWeatherActivity.java
+++ trunk/AndroidWeather/src/com/ii/androidweather/AndroidWeatherActivity.java
@@ -3,6 +3,10 @@ package com.ii.androidweather;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -10,19 +14,66 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class AndroidWeatherActivity extends Activity {
-    public static final String SERVICE_ENDPOINT="http://api.wunderground." +
-    		"com/auto/wui/geo/GeoLookupXML/index.xml?query=";
+    
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState); 
+        setContentView(R.layout.main);
+        /* Create a new TextView to display the parsingresult later. */ 
+        
+        try { 
+             /* Create a URL we want to load some xml-data from. */ 
+        	 URL url = new URL(WeatherUtils.url); 
+
+             /* Get a SAXParser from the SAXPArserFactory. */ 
+             SAXParserFactory spf = SAXParserFactory.newInstance(); 
+             SAXParser sp = spf.newSAXParser(); 
+
+             /* Get the XMLReader of the SAXParser we created. */ 
+             XMLReader xr = sp.getXMLReader(); 
+             /* Create a new ContentHandler and apply it to the XML-Reader*/ 
+             GoogleWeatherHandler WeatherHandler = new GoogleWeatherHandler(); 
+             xr.setContentHandler(WeatherHandler); 
+              
+             /* Parse the xml-data from our URL. */ 
+             xr.parse(new InputSource(url.openStream())); 
+             /* Parsing has finished. */ 
+
+             /* Our ExampleHandler now provides the parsed data to us. */ 
+             WeatherSet parsedExampleDataSet = 
+                                           WeatherHandler.getWeatherSet(); 
+             TextView sostojbaFld=(TextView)findViewById(R.id.state);
+             sostojbaFld.setText(parsedExampleDataSet.getWeatherCurrentCondition().getCondition());
+             TextView temFld=(TextView)findViewById(R.id.tempText);
+            temFld.setText(parsedExampleDataSet.getWeatherCurrentCondition().getTempCelcius().toString());
+             /* Set the result to be displayed in our GUI. */ 
+            // tv.setText(parsedExampleDataSet.getWeatherCurrentCondition().getCondition()); 
+              
+        } catch (Exception e) { 
+             /* Display any Error to the GUI. */ 
+            // tv.setText("Error: " + e.getMessage()); 
+            
+        } 
+        /* Display the TextView. */ 
+      //  this.setContentView(tv); 
+   } 
 	//metoda koja sluzi za prakanje na query do web servisot 
+    
     public void queryWS(String query)
     {
     HttpClient httpClient = new DefaultHttpClient();
-	HttpGet request = new HttpGet(SERVICE_ENDPOINT+query);
+	HttpGet request = new HttpGet(WeatherUtils.SERVICE_ENDPOINT+query);
 	HttpResponse response;
 	try {
 		response = httpClient.execute(request);
@@ -49,10 +100,5 @@ public class AndroidWeatherActivity extends Activity {
 	
 
     }	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+    
 }
